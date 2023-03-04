4c3c8de3d78168db2ca4b9ab2ea0a7600af8d596
El-Shonkee <108562297+El-Shonkee@users.noreply.github.com>
2023-03-04 18:24:28 -0500

    Add files via upload

---
+++ src/main/java/Model/Plans.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author andrea.correaa1
+ */
+public class Plans {
+    int idPlan;
+    String namePlan;
+    String descriptionPlan;
+    String days;
+    String hours;
+    String duration;
+    int pricePlan;
+    int accountingPeople;
+    double priceSubscription;
+    double priceAdultTicket;
+    double priceChildTicket;
+    String souvenir;
+    double priceSouvenir;
+    
+}
---
+++ src/main/java/View/Zoologico.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package View;
+
+/**
+ *
+ * @author Julian Isaza Marin
+ */
+public class Zoologico {
+
+    public static void main(String[] args) {
+        MENU N = new MENU();
+        N.setView(true);
+    }
+}
